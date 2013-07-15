43ddd8fd51bda6b496ef7712077ded2c4b0523a8
bj <britneyjo.ludkowski@gmail.com>
2013-07-15 22:30:45 +0100

    bootstrap added, edit views and form added

--- app/models/music_track.rb
+++ app/models/music_track.rb
@@ -1,5 +1,5 @@
 class MusicTrack < ActiveRecord::Base
-  attr_accessible :name, :release_date, :description, :track_file, :user, :user_id
+  attr_accessible :name, :release_date, :description, :track_file, :user, :user_id, :genre_ids
   has_and_belongs_to_many :genres
   belongs_to :user
   has_many :comments
